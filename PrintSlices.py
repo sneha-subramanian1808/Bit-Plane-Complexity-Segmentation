9926d5d6eb2d3d4fc645009744f71e8b529b71c9
Sneha Subramanian <98036669+sneha-subramanian1808@users.noreply.github.com>
2022-09-13 18:47:17 +0400

    Update PrintSlices.py

--- PrintSlices.py
+++ PrintSlices.py
@@ -1,3 +1,4 @@
+#Importing needed libraries
 import argparse
 import numpy
 from PIL import Image
@@ -6,13 +7,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--infile', type=str, help='path to vessel image (.png)')
 opts = parser.parse_args()
 
-#Load Image
+#Loading the image
 image = Image.open(opts.infile).convert('L')
 
-#Convert Image to Array
+#Converting the image to an array
 array = numpy.array(image)
 
-#Slice BitPlane
+#Slicing the BitPlane
 print('Slicing...')
 bitPlaneArr  = numpy.zeros( (array.shape[0], array.shape[1], 8), dtype = 'uint8' )
 bitPlaneArr[:,:,0] = numpy.copy(array)
@@ -23,7 +24,7 @@ for i in range(bitPlaneArr.shape[0]):
             bitPlaneArr[i,j,k] =  bitArr[k]
 print('Sliced.')
 
-#print all by creating a copy of bitmatrix and making each plane of the copy a 8 bit image
+#Printing all the BitPlanes by creating a copy of BitMatrix and making each plane copy a 8 bit image
 print('Displaying all bitplanes in order of most to least signifigant')
 tmparr = numpy.copy(bitPlaneArr)
 for k in range(8):
